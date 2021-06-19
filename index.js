a558cc2de245573d2f196cad5b015a61e11857eb
Tamilkumar27 <tamilkumar2710@gmail.com>
2021-06-19 17:58:02 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert("This Webiste is only for Mobile Screen. For other screen, website is Under Construction. ");
+setTimeout( function(){
+    alert("Note : In this page, Form will not work so use Phone number or E-mail to contact Me.");
+}, 1000);
+ 
+ 
