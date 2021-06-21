48902e6f442eb6c77350ee39bd62f3e8d7087b24
tamilkumar <81823415+Tamilkumar27@users.noreply.github.com>
2021-06-21 13:24:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// setTimeout( function(){
-//     alert("Note : In this page, Form will not work so use Phone number or E-mail to contact Me.");
-// }, 1000);
+setTimeout( function(){
+    alert("Note : In this page, Form will not work so use Phone number or E-mail to contact Me.");
+}, 1000);
  
  
